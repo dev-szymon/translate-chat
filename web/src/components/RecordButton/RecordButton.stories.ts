0872f6e3c4559bba4a248720a9b08938444a2bd0
Szymon Gazda <szymongazda95@gmail.com>
2023-11-10 15:33:43 +0100

    chore(web): Add RecordButton variants, rename SpeechBubble default story

--- web/src/components/RecordButton/RecordButton.stories.ts
+++ web/src/components/RecordButton/RecordButton.stories.ts
@@ -17,8 +17,20 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const Idle: Story = {
     args: {
         variant: "idle"
+    }
+};
+
+export const Recording: Story = {
+    args: {
+        variant: "recording"
+    }
+};
+
+export const Pending: Story = {
+    args: {
+        variant: "pending"
     }
 };
